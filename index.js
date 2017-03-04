974be82c611ac8c925c9afe0c6c570281323b059
Petar Kolaric <petarkolaric@users.noreply.github.com>
2017-03-04 23:49:16 +1100

    remove evidence of copy paste development

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ app.post('/jomelify', function (req, res) {
   if (!req.files)
   return res.status(400).send('No files were uploaded.')
 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let displayImage = req.files.displayImage
 
-  // Use the mv() method to place the file somewhere on your server
   displayImage.mv('/app/uploads/input', function(err) {
     if (err)
     return res.status(500).send(err)
